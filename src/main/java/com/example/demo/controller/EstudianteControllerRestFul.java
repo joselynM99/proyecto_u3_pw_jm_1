fc3d7e0f3e0e6e56bc90b5d394418fc626a02848
joselynM99 <joselynmoncayo1999@gmail.com>
2023-02-04 10:53:37 -0500

    taller27

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,35 +15,49 @@ import com.example.demo.service.IEstudianteService;
 @RestController
 @RequestMapping("/estudiantes")
 public class EstudianteControllerRestFul {
-	
+
 	@Autowired
 	private IEstudianteService estudianteService;
 
+	@PostMapping
 	public void registrar(Estudiante estudiante) {
 
 	}
 
-	public void actualizar(Estudiante estudiante) {
+	@PutMapping(path = "/{id}")
+	public void actualizar(@PathVariable("id") Integer id, Estudiante estudiante) {
 
 	}
 
-	@GetMapping(path = "/buscar/{id}")
+	@PutMapping
+	public void actualizarTodos(Estudiante estudiante) {
+
+	}
+
+	@GetMapping(path = "/{id}")
 	public Estudiante encontrar(@PathVariable("id") Integer id) {
 		return this.estudianteService.encontrar(id);
 	}
-	
-	
+
 	@GetMapping(path = "/buscarPorNombre/{nombre}")
 	public Estudiante encontrarEstudianteNombre(@PathVariable("nombre") String nombre) {
 		return this.estudianteService.encontrarEstudiantePorNombre(nombre);
 	}
-	
+
 	@GetMapping(path = "/buscarPorApellido/{apellido}")
 	public Estudiante encontrarEstudianteApellido(@PathVariable("apellido") String apellido) {
 		return this.estudianteService.encontrarEstudiantePorApellido(apellido);
 	}
 
-	public void borrar(Integer id) {
+	@DeleteMapping(path = "/{id}")
+	public void borrar(@PathVariable("id") Integer id) {
+		
+		this.estudianteService.borrar(id);
+
+	}
+	
+	@DeleteMapping
+	public void borrarTodos() {
 
 	}
 
